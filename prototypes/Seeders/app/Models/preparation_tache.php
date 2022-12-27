1fbe8b27a12a300f4403f16a1d63050818c6422b
Mohamed yahya <92155140+Mohamed-Yahyaa@users.noreply.github.com>
2022-12-27 10:58:18 +0100

    Seeders Model and factory

---
+++ prototypes/Seeders/app/Models/preparation_tache.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class preparation_tache extends Model
+{
+    use HasFactory;
+}
