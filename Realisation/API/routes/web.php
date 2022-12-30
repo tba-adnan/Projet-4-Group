d37dab0214dcd245bc047e30feaca1e25d02c3d4
Mohamed yahya <92155140+Mohamed-Yahyaa@users.noreply.github.com>
2022-12-30 16:59:48 +0100

    Master update change

--- Realisation/API/routes/web.php
+++ Realisation/API/routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    redirect('/task');
 });
 
 Route::resource('task', PreparationTacheController::class);
