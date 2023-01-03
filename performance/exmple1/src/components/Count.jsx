2f74aa1aca9855f8aaa9fe26bbfdfd939da40a74
stitou-nada <nadastito1087@gmail.com>
2023-01-03 09:36:18 +0100

    count

--- performance/exmple1/src/components/Count.jsx
+++ performance/exmple1/src/components/Count.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 export const Count = React.memo(({ onOdd }) => {
-    console.log("render");
+  console.log("render");
   const [count, setCount] = React.useState(0);
   const renders = React.useRef(0);
-  return (
+    return (
     <div className="App">
       <div>count: {count}</div>
       <div>renders: {renders.current++}</div>
