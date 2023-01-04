b1ac2a971e8bf26b434af4c97067ab09ed30dc21
EL_MLIKI_HICHAM <elmliki.hicham@gmail.com>
2023-01-05 00:07:42 +0100

    integration dashboard react

--- Realisation/API/react/src/App.jsx
+++ Realisation/API/react/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
+import './App.css';
+import Header from './components/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-    hello
+    <div>
+      <Header />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Realisation/API/routes/api.php
+++ Realisation/API/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::controller(DashboardController::class)->group(function(){
+    Route::get('/group/{id}', 'formation');
+    Route::get('/group', 'years');
+    Route::get('/studentAv', 'studentAv');
+    // Route::get('/lastY', 'lastYear');
 });
