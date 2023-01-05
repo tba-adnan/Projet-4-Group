28c4a98d4340fe455a429fa351ef21ccc3491bc0
EL_MLIKI_HICHAM <elmliki.hicham@gmail.com>
2023-01-05 20:35:48 +0100

    dashboard

--- Realisation/API/resources/views/dashboard.blade.php
+++ Realisation/API/resources/views/dashboard.blade.php
@@ -1,16 +1,8 @@
 @extends('master')
 @section('content')
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>{{__('message.title')}}</h1>
-          </div>
-        </div>
-      </div>
-    </section>
+    <!-- dashboard -->
+
 <div class="container-xl">
 
 <div id="root"></div>
