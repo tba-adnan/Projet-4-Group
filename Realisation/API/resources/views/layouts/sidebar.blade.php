d7e9bf5d1207d1bfc149b94c2bd9600d6412eae8
Mohamed yahya <92155140+Mohamed-Yahyaa@users.noreply.github.com>
2023-01-05 13:07:03 +0100

    Update : add Button Dashboard with sidebar

--- Realisation/API/resources/views/layouts/sidebar.blade.php
+++ Realisation/API/resources/views/layouts/sidebar.blade.php
@@ -35,6 +35,22 @@
             </p>
           </a>
 
+          <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+              <!-- Add icons to the links using the .nav-icon class
+                   with font-awesome or any other icon font library -->
+      
+              {{-- <li class="nav-item menu-open"> --}}
+                <a href="/" class="nav-link active">
+                  <i class="nav-icon fas fa-table"></i>
+                  <p>
+                    {{__('message.dashboard')}}
+                    {{-- <i class="fas fa-angle-left right"></i> --}}
+                  </p>
+                </a>
+
+          
+
 
       </ul>
     </nav>
